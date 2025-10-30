213b125ff44052e28a124ab971548303d755f878
IvanKircsanov <i.kircsanov@gmail.com>
2025-10-30 15:48:29 +0100

    18.10

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// let x = 10
+// let y = 5
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// let number = 10
+// number = number + 20
+// number += 20
+// number -= 20;
+// number *= 20;
+// number /= 20;
+// number %= 20;
+// console.log(number);
+
+// const one = 4
+// const two = 8
+// const three = 4
+// console.log(one <= three);
+
+// 3 ( === ) : strogoe ravestwa
+// 2 ( == ) : ne strogoe ravestwa
